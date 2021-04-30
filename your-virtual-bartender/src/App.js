2adb763eeb7077ce05ae200f0af679ac847a5c3a
kortmiranda <kortneymiranda@outlook.com>
2021-04-29 21:16:40 -0500

    search input is printing out in console log

--- your-virtual-bartender/src/App.js
+++ your-virtual-bartender/src/App.js
@@ -1,24 +1,77 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 // import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
 import Header from './components/Header'
 import RandomDrink from './components/RandomDrink'
-import Result from './components/Result'
+import SearchResult from './components/SearchResult'
 import SearchForm from './components/SearchForm'
-import Error from './components/Error'
+import Error from './pages/Error'
 import { Route } from 'react-router-dom'
 
-
-
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [drink, setDrink] = useState()
   
   
+  useEffect(() => {
+    getDrink(searchTerm)
+  }, [])
+  
+  function getDrink(searchTerm) {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+    fetch(url)
+    
+    .then(response => response.json())
+    .then(response => {
+      setDrink(response.drinks)
+      const {drinks} = response
+      if (drinks) {
+        const newDrink = drinks.map((item) => {
+          const {
+            idDrink,
+            strDrink,
+            strDrinkThumb,
+            strAlcoholic,
+            strGlass,
+          } = item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setDrink(newDrink);
+      } else {
+        setDrink([]);
+      }
+      console.log(response.drinks)
+    })
+    .catch(console.error)
+  }
+
+  function handleChange(event) {
+    setSearchTerm(event.target.value)
+  }
+  function handleSubmit(event) {
+    event.preventDefault()
+    getDrink(searchTerm)
+  }
+  
+
   return (
+
     <div className="App">
+      <SearchForm handleChange={handleChange}
+                handleSubmit={handleSubmit}
+                searchTerm={searchTerm}/>
       <Route exact path = "/" component={Header} />
       <Route exact path = "/recommendation/" component={RandomDrink}/>
       <Route exact path = "/drink-search" component={SearchForm} />
-      <Route exact path = "/drink-result" component={Result} />
+      <Route exact path = "/drink-result/:name">
+        <SearchResult drink={drink}/>
+      </Route>
       <Route exact path = "/drink-result-error" component={Error} />
     </div>
   );
